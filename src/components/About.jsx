3150951e2f76885bdd119d796505ebe88f50113e
Stephanie Xue <stephaniexue3@gmail.com>
2024-10-02 23:16:13 -0700

    adjust about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ function About() {
         <div className="about-page">
             <h1>About</h1>
             <p>
-                I am an aspiring software developer with a previous background in the healthcare industry as a pharmacist. I have particular interest in healthcare technology and am passionate about levaraging technology to help automate tasks, bridge gaps, and develop innovative solutions. As a avid learner, I am always eager to explore new ideas, expand my skillset, and challenge myself to grow both personally and professionally.
+                I am an aspiring software developer with a previous background in the healthcare industry as a pharmacist. I am passionate about levaraging technology to help automate tasks, bridge gaps, and develop innovative solutions. As a avid learner, I am always eager to explore new ideas, expand my skillset, and challenge myself to grow both personally and professionally.
             </p>
         </div>
     );
